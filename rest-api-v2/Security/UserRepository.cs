6d34ef48f82f545e9ce15ab089f401c504a7c37f
riz-j <adiputra979@gmail.com>
2023-01-29 15:52:44 +0700

    Security: Generate Token on Successful Login (Function)

--- rest-api-v2/Security/UserRepository.cs
+++ rest-api-v2/Security/UserRepository.cs
@@ -3,15 +3,21 @@ using rest_api_v2.Models;
 using rest_api_v2.Security.Interfaces;
 using rest_api_v2.Security.Models;
 using rest_api_v2.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
 
 namespace rest_api_v2.Security;
 
 public class UserRepository : IUserRepository
 {
     private AppDbContext _db;
-    public UserRepository(AppDbContext db)
+    private string secretKey;
+    public UserRepository(AppDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:Secret");
     }
 
     public bool IsUniqueUser(string email)
@@ -28,7 +34,38 @@ public class UserRepository : IUserRepository
 
     public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
     {
-        throw new NotImplementedException();
+        var _user = _db.Users.FirstOrDefault(u => u.Email.ToLower() == loginRequestDTO.Email.ToLower()
+                                            && u.Password == loginRequestDTO.Password);
+        
+        if (_user == null)
+        {
+            return null;
+        }
+
+        // If user was found, generate JWT token:
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, _user.Id.ToString()),
+                new Claim(ClaimTypes.Email, _user.Email.ToString()),
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new(new SymmetricSecurityKey(key), 
+                                     SecurityAlgorithms.HmacSha256Signature)
+        };
+        
+        var _token = tokenHandler.CreateToken(tokenDescriptor);
+
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+        {
+            User = _user,
+            Token = tokenHandler.WriteToken(_token)
+        };
+        return loginResponseDTO;
     }
 
     public async Task<User> Register(RegistrationRequestDTO registrationRequestDTO)
