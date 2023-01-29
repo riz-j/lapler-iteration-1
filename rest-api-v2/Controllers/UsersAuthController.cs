feddffa9b8a9105d2bf83bca76db4458f42f695d
riz-j <adiputra979@gmail.com>
2023-01-29 16:21:24 +0700

    Security: Create API endpoint for User Registration

--- rest-api-v2/Controllers/UsersAuthController.cs
+++ rest-api-v2/Controllers/UsersAuthController.cs
@@ -25,4 +25,23 @@ public class UsersAuthController : ControllerBase
         }
         return Ok(_loginResponse);
     }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody]RegistrationRequestDTO registrationRequestDTO)
+    {
+        bool _isUserUnique = _userRepository.IsUniqueUser(registrationRequestDTO.Email);
+        if (_isUserUnique == false)
+        {
+            return BadRequest(new { message = "Email is already registered" });
+        }
+
+        var _user = await _userRepository.Register(registrationRequestDTO);
+        
+        if (_user == null)
+        {
+            return BadRequest();
+        }
+
+        return Ok(new { message = "User successfully registered" } );
+    }
 }
