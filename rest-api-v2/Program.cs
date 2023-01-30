7a69be43eb73e1c50b73012b3b595538555a4a38
riz-j <adiputra979@gmail.com>
2023-01-30 14:51:59 +0700

    Security: Add configuration for OpenAPI Swagger Authorization

--- rest-api-v2/Program.cs
+++ rest-api-v2/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using rest_api_v2.Data;
 using rest_api_v2.Security;
 using rest_api_v2.Security.Interfaces;
@@ -37,7 +38,32 @@ builder.Services.AddAuthentication(x => {
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme 
+    {
+        Description = "JWT Authorization header uses the bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement() 
+    {
+        { 
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddTransient<UsersService>();
 builder.Services.AddTransient<ProjectsService>();
