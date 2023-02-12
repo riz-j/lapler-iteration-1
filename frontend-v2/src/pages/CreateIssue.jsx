41e409f21a73b7b4d3ddf62ceabc9ac1157b3d5b
riz-j <adiputra979@gmail.com>
2023-02-12 23:15:39 +1100

    Tidy up the add Create New Issue page

--- frontend-v2/src/pages/CreateIssue.jsx
+++ frontend-v2/src/pages/CreateIssue.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { createIssue } from "../redux/currentProjectSlice";
 
 export default function CreateIssue() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const currentUser = useSelector(state => state.currentUser);
     const { projectId } = useParams();
-    const [typeOfIssue, setTypeOfIssue] = useState('');
-    const [priorityOfIssue, setPriorityOfIssue] = useState('');
-    const [statusOfIssue, setStatusOfIssue] = useState('');
+    const [typeOfIssue, setTypeOfIssue] = useState('Bug');
+    const [priorityOfIssue, setPriorityOfIssue] = useState('Low');
+    const [statusOfIssue, setStatusOfIssue] = useState('Waiting');
     const [summary, setSummary] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        navigate(-1);
         dispatch(createIssue({
             token: currentUser.token,
             projectId: projectId,
@@ -21,16 +23,37 @@ export default function CreateIssue() {
             priorityOfIssue: priorityOfIssue,
             statusOfIssue: statusOfIssue,
             summary: summary
-        }))
+        }));
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="typeOfIssue" onChange={e => setTypeOfIssue(e.target.value)} />
-            <input type="text" placeholder="priorityOfIssue" onChange={e => setPriorityOfIssue(e.target.value)} />
-            <input type="text" placeholder="statusOfIssue" onChange={e => setStatusOfIssue(e.target.value)} />
-            <input type="text" placeholder="summary" onChange={e => setSummary(e.target.value)} />
-            <input type="submit" />
-        </form>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-2xl pb-4">Create New Issue</h1>
+            <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4 w-1/3 justify-center justify-items-center">
+                <select type="text" placeholder="typeOfIssue" className="border-2 border-black px-2 py-1 rounded-md w-full"
+                onChange={e => setTypeOfIssue(e.target.value)} >
+                    <option value="Bug">Bug</option>
+                    <option value="Improvement">Improvement</option>
+                    <option value="New Feature">New Feature</option>
+                    <option value="Epic">Epic</option>
+                    <option value="Idea">Idea</option>
+                </select>
+                <select type="text" placeholder="priorityOfIssue" className="border-2 border-black px-2 py-1 rounded-md w-full"
+                onChange={e => setPriorityOfIssue(e.target.value)} >
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+                <select type="text" placeholder="statusOfIssue" className="border-2 border-black px-2 py-1 rounded-md w-full"
+                onChange={e => setStatusOfIssue(e.target.value)} >
+                    <option value="Waiting">Waiting</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                </select>
+                <input type="text" placeholder="summary" className="border-2 border-black px-2 py-1 rounded-md w-full"
+                onChange={e => setSummary(e.target.value)} />
+                <input type="submit" className="border-2 border-black px-2 py-1 rounded-md w-3/4" />
+            </form>
+        </div>
     )
 }
