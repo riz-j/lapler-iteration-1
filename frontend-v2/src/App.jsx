48a745787c7ee781b08904c15c7992d35d014c35
riz-j <adiputra979@gmail.com>
2023-02-17 19:31:14 +1100

    Clean up code: App.jsx and main.jsx

--- frontend-v2/src/App.jsx
+++ frontend-v2/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import CreateProject from './pages/CreateProject';
 import CreateIssue from './pages/CreateIssue';
 import UpdateIssue from './pages/UpdateIssue';
@@ -13,14 +13,14 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/register" element={ <Register /> } />
-        <Route path="project/new" element={ <CreateProject /> } />
-        <Route path="dashboard/project/:projectId" element={ <Dashboard /> } />
-        <Route path="dashboard/project/:projectId/issues/new" element={ <CreateIssue /> } />
-        <Route path="dashboard/project/:projectId/issues/:issueId/update" element={ <UpdateIssue /> } />
-        <Route path="dashboard/project/:projectId/users/add" element={ <AddUserToProject /> } />
+        <Route path='/' element={ <Home /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/register' element={ <Register /> } />
+        <Route path='project/new' element={ <CreateProject /> } />
+        <Route path='dashboard/project/:projectId' element={ <Dashboard /> } />
+        <Route path='dashboard/project/:projectId/issues/new' element={ <CreateIssue /> } />
+        <Route path='dashboard/project/:projectId/issues/:issueId/update' element={ <UpdateIssue /> } />
+        <Route path='dashboard/project/:projectId/users/add' element={ <AddUserToProject /> } />
       </Routes>
     </div>
   )
--- frontend-v2/src/main.jsx
+++ frontend-v2/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
