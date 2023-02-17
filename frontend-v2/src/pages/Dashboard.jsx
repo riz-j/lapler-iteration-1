d3c2e383b69ec8fc799745adbab36c7873e126b1
riz-j <adiputra979@gmail.com>
2023-02-17 18:48:39 +1100

    Clean up code: Dashboard.jsx

--- frontend-v2/src/pages/Dashboard.jsx
+++ frontend-v2/src/pages/Dashboard.jsx
@@ -1,50 +1,50 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom"
-import { emptyCurrentProject, getCurrentProject } from "../redux/currentProjectSlice";
-import IssueCard from "../components/IssueCard";
-import ProjectUserCard from "../components/ProjectUserCard";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom'
+import { emptyCurrentProject, getCurrentProject } from '../redux/currentProjectSlice';
+
+import IssueCard from '../components/IssueCard';
+import ProjectUserCard from '../components/ProjectUserCard';
 
 export default function Dashboard() {
     const { projectId } = useParams();
     const dispatch = useDispatch();
-    const currentUser = useSelector(state => state.currentUser);
     const currentProject = useSelector(state => state.currentProject);
+    const currentUser = useSelector(state => state.currentUser);
+    const token = currentUser.token;
     
-    const currentProjectId = () => {              //
-      const url = window.location.pathname;       //  Need to fix this by fixing the 
-      const id = url.split("/").slice(-1)[0];     //  getCurrentProject()  to have the 
-      return id;                                  //  Project ID in the redux state.
-    };                                            //
-
     useEffect(() => {
         dispatch(emptyCurrentProject());
         dispatch(getCurrentProject({ 
             projectId: projectId, 
-            token: currentUser.token 
+            token: token 
         }));
-    }, [])
+    }, [dispatch, projectId, token])
 
     return (
-        <div className="flex h-screen sm:text-sm">
-          <div className="bg-green-300 w-80 hidden md:flex flex-col pt-4">
-            <p className="bg-blue-200 rounded-lg px-5 py-2 border-b-2">All states</p>
-            <p className="bg-blue-200 rounded-lg px-5 py-2 border-b-2">Some states</p>
-            <p className="bg-blue-200 rounded-lg px-5 py-2 border-b-2">Few states</p>
-            <Link to="/">
-              <p className="bg-blue-200 rounded-lg px-5 py-2 border-b-2 mt-96">Home</p>
+        <div className='flex h-screen sm:text-sm'>
+          <div className='bg-green-300 w-80 hidden md:flex flex-col pt-4'>
+            <p className='bg-blue-200 rounded-lg px-5 py-2 border-b-2'>All states</p>
+            <p className='bg-blue-200 rounded-lg px-5 py-2 border-b-2'>Some states</p>
+            <p className='bg-blue-200 rounded-lg px-5 py-2 border-b-2'>Few states</p>
+            <Link to='/'>
+              <p className='bg-blue-200 rounded-lg px-5 py-2 border-b-2 mt-96'>Home</p>
             </Link>
           </div>
       
-          <div className="w-full bg-yellow-200">
-            <div className="bg-green-200 flex justify-evenly items-center h-8">
-              <input className="w-1/2 h-6" />
-              <Link to="issues/new">
+          <div className='w-full bg-yellow-200'>
+            <div className='bg-green-200 flex justify-evenly items-center h-8'>
+              
+              <input className='w-1/2 h-6' />
+              
+              <Link to='issues/new'>
                 <p>New Issue</p>
               </Link>
-              <Link to="users/add">
+
+              <Link to='users/add'>
                 <p>Add User</p>
               </Link>
+
             </div>
       
             <div>
@@ -68,12 +68,14 @@ export default function Dashboard() {
             </div>
           </div>
       
-          <div className="bg-red-200 w-80 hidden md:block">
-            { (currentProject.users) ? 
-              (currentProject.users).map(user => 
-                // <p className="bg-blue-200 rounded-lg px-5 py-2 border-b-2">{user.firstName} {user.lastName}</p>
-                <ProjectUserCard userId={user.id} userFirstName={user.firstName} userLastName={user.lastName} />
-              ) : <></>
+          <div className='bg-red-200 w-80 hidden md:block'>
+            { (currentProject.users) && (currentProject.users).map(user => 
+                <ProjectUserCard 
+                  userId={user.id} 
+                  userFirstName={user.firstName} 
+                  userLastName={user.lastName} 
+                />
+              ) 
             }
           </div>
         </div>
