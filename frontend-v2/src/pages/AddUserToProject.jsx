2e58143071f09f7281bdd6a65d4a1e0b5e640be8
riz-j <adiputra979@gmail.com>
2023-02-17 19:03:46 +1100

    Clean up code: AddUserToProject.jsx

--- frontend-v2/src/pages/AddUserToProject.jsx
+++ frontend-v2/src/pages/AddUserToProject.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom"
-import { addUsersToProject, getCurrentProject } from "../redux/currentProjectSlice";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
+import { addUsersToProject, getCurrentProject } from '../redux/currentProjectSlice';
 
 export default function AddUserToProject() {
-    const { projectId } = useParams();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const currentProject = useSelector(state => state.currentProject);
     const token = useSelector(state => state.currentUser.token);
-    const dispatch = useDispatch();
+    const { projectId } = useParams();
+    
     const [emailList, setEmailList] = useState([]);
     const [email, setEmail] = useState('');
 
@@ -19,6 +20,7 @@ export default function AddUserToProject() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         navigate(-1);
+
         await dispatch(addUsersToProject({
             projectId: projectId,
             emailList: emailList,
@@ -31,22 +33,41 @@ export default function AddUserToProject() {
     }
 
     return (
-        <div className="flex flex-col h-screen w-screen justify-center items-center">
-            <h1 className="text-xl mb-4">Add users to project {projectId}</h1>
+        <div className='flex flex-col h-screen w-screen justify-center items-center'>
+
+            <h1 className='text-xl mb-4'>Add users to project {projectId}</h1>
+            
             { emailList &&
                 emailList.map(email => <h1>{email}</h1>)
             }
 
-            <div className="flex">
-                <input type="text" className="border-2 border-black px-2 py-1 rounded-lg"
-                onChange={e => setEmail(e.target.value)}/>
-                <button type="submit" className="border-2 border-black px-2 py-1 rounded-lg"
-                onClick={handleAddEmail}>Add</button>
+            <div className='flex'>
+
+                <input 
+                    type='text' 
+                    placeholder='email'
+                    onChange={e => setEmail(e.target.value)}
+                    className='border-2 border-black px-2 py-1 rounded-lg'
+                />
+
+                <button 
+                    type='submit' 
+                    className='border-2 border-black px-2 py-1 rounded-lg'
+                    onClick={handleAddEmail}
+                >Add</button>
+
             </div>
 
-            <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">  
-                <input type="submit" className="border-2 border-black px-2 py-1 rounded-lg" />
+            <form 
+                onSubmit={handleSubmit} 
+                className='grid grid-cols-1 gap-4'
+            >  
+                <input 
+                    type='submit' 
+                    className='border-2 border-black px-2 py-1 rounded-lg' 
+                />
             </form>
+
         </div>
     )
 }
