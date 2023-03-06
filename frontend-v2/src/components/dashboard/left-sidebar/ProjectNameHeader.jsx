3681bd5891c3f6604247fb4d7f000397be0e2b97
riz-j <adiputra979@gmail.com>
2023-03-06 17:26:07 +1100

    display project name on top of dashboard sidebar

--- frontend-v2/src/components/dashboard/left-sidebar/ProjectNameHeader.jsx
+++ frontend-v2/src/components/dashboard/left-sidebar/ProjectNameHeader.jsx
@@ -1,14 +1,17 @@
+import { useSelector } from 'react-redux';
 import ProjectProfilePicture from '../../../static/img/IMG_2352.png';
 import ProjectSettingsIcon from '../../../static/img/ProjectSettingsIcon.png'
 
 export default function ProjectNameHeader() {
+    const currentProject = useSelector(state => state.currentProject);
+
     return (
         <div>
             <div className='flex justify-between  items-center h-12 w-full top-0 left-0'>
                 <div className='flex justify-start items-center gap-2 ml-4'> 
                     <img src={ProjectProfilePicture} className='w-6 h-6 rounded' />
-                    <h1 className='truncate text-md font-semibold text-[#FCFDFF]'>
-                        Tensor Lite
+                    <h1 className='truncate overflow-hidden text-md font-semibold text-[#FCFDFF] w-32'>
+                        {currentProject.projectName}
                     </h1>
                 </div>
                 <div>
