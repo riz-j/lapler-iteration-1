65a6c5028284b06b4e4721516ce8867b2b31f05d
riz-j <adiputra979@gmail.com>
2023-03-26 17:02:02 +1100

    Add basic collapsable sidebar button and implement its functionality

--- frontend-v2/src/pages/Dashboard.jsx
+++ frontend-v2/src/pages/Dashboard.jsx
@@ -1,77 +1,45 @@
+import { useState } from 'react';
 import LeftSidebar from '../components/dashboard/left-sidebar';
 import ProjectNavigation from '../components/dashboard/project-navigation';
 import MainSection from '../components/main-section/issues/index.jsx';
 
 export default function Dashboard() {
-    // const { projectId } = useParams();
-    // const dispatch = useDispatch();
-    // const currentProject = useSelector(state => state.currentProject);
-    // const currentUser = useSelector(state => state.currentUser);
-    // const token = currentUser.token;
-    
-    // useEffect(() => {
-    //     dispatch(emptyCurrentProject());
-    //     dispatch(getCurrentProject({ 
-    //         projectId: projectId, 
-    //         token: token 
-    //     }));
-    // }, [dispatch, projectId, token])
+    const [show, setShow] = useState(true);
+
+    function renderCloseButton() {
+      return (
+          <button
+            onClick={() => setShow(!show)}
+            className="absolute bottom-[30vh] left-[17rem] z-10 bg-platinum-tertiary opacity-40 hover:opacity-100 w-4 h-16 border-r border-t border-b hover:border-l border-gray-400 hover:border-white flex items-center justify-center rounded-r-2xl"
+          >
+            {'<'}
+          </button> 
+      )
+    }
+
+      function renderOpenButton() {
+        return (
+            <button
+              onClick={() => setShow(!show)}
+              className="absolute bottom-[30vh] left-0 z-10 bg-platinum-tertiary opacity-40 hover:opacity-100 w-4 h-16 border-r border-t border-b hover:border-l border-gray-400 hover:border-white flex items-center justify-center rounded-r-2xl"
+            >
+              {'>'}
+            </button> 
+        )
+    }
 
     return (
         <div className='flex h-screen sm:text-sm text-font-color-primary'>
-          <ProjectNavigation/>
-          <LeftSidebar />
+          
+          <div className={`flex ${!show && 'hidden'}`}>
+            <ProjectNavigation />
+            <LeftSidebar />
+          </div>
 
+          { show ? renderCloseButton() : renderOpenButton() }  
+          
           <MainSection />
-      
-          {/* <div className='grow w-full bg-yellow-200'>
-            <div className='bg-green-200 flex justify-evenly items-center h-8'>
-              
-              <input className='w-1/2 h-6' />
-              
-              <Link to='issues/new'>
-                <p>New Issue</p>
-              </Link>
 
-              <Link to='users/add'>
-                <p>Add User</p>
-              </Link>
-
-            </div>
-      
-            <div>
-              {(currentProject.issues && currentProject.issues.length > 0) ? (
-                currentProject.issues.map(issue => (
-                  <IssueCard 
-                    projectId={projectId}
-                    issueId={issue.id} 
-                    typeOfIssue={issue.typeOfIssue} 
-                    priorityOfIssue={issue.priorityOfIssue} 
-                    statusOfIssue={issue.statusOfIssue} 
-                    summary={issue.summary}
-                    dueDate={issue.dueDate}
-                    assigneeId={issue.assigneeId}
-                    reporterId={issue.reporterId}
-                  /> 
-                ))
-              ) : (
-                <h1>This project is empty</h1>
-              )}
-            </div>
-          </div> */}
-      
-                {/*    Map Users :    */}
-
-          {/* <div className='bg-red-200 w-80 hidden md:block'>
-            { (currentProject.users) && (currentProject.users).map(user => 
-                <ProjectUserCard 
-                  userId={user.id} 
-                  userFirstName={user.firstName} 
-                  userLastName={user.lastName} 
-                />
-              ) 
-            }
-          </div> */}
         </div>
       );
 }
