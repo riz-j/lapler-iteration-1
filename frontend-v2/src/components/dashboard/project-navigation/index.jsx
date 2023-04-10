5a3a2fbf2c555974bc449abae297e052c78fc3f4
riz-j <adiputra979@gmail.com>
2023-04-10 20:50:22 +1000

    Code cleanup

--- frontend-v2/src/components/dashboard/project-navigation/index.jsx
+++ frontend-v2/src/components/dashboard/project-navigation/index.jsx
@@ -38,26 +38,6 @@ export default function ProjectNavigation() {
                         </Link>
                     ))
                     }
-
-                    {/* { currentUser.projects && (
-                    Object.entries(currentUser.projects).map(([projectId, projectName]) => 
-                        <Link to={`/dashboard/project/${projectId}`} className="flex justify-center items-center h-12 w-12">
-                            <img key={projectId} src={ProjectProfilePic1} className='w-2/3 h-2/3 rounded-full'/>
-                        </Link>
-                    ))
-                    } */}
-
-                    {/* <div className="flex justify-center items-center h-12 w-full">
-                        <img src={ProjectProfilePic4} className='w-2/3 h-2/3 rounded-full'/>
-                    </div>  
-
-                    <div className="flex justify-center items-center h-12 w-full">
-                        <img src={ProjectProfilePic3} className='w-2/3 h-2/3 rounded-full'/>
-                    </div>  
-
-                    <div className="flex justify-center items-center h-12 w-full">
-                        <img src={ProjectProfilePic2} className='w-2/3 h-2/3 rounded-full'/>
-                    </div>   */}
                     
                     { currentUser.projects &&
                         <hr className='border-2 mx-2.5 rounded my-2 border-[#3e3f43]' />
