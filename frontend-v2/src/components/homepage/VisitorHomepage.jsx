fe48584ec170745fdaa863ada27f6a9caa5969d0
riz-j <adiputra979@gmail.com>
2023-04-24 19:04:06 +1000

    Responsive for Landing Page complete

--- frontend-v2/src/components/homepage/VisitorHomepage.jsx
+++ frontend-v2/src/components/homepage/VisitorHomepage.jsx
@@ -7,11 +7,11 @@ import github_logo from '../../static/img/homepage/github-logo.png'
 export default function VisitorHomepage() {
     return (
         <div className='flex flex-col'>
-            <div className='flex justify-between items-center px-[10vw] bg-black w-full h-[20vh]'>
+            <div className='flex justify-between items-center px-[10vw] bg-black w-full h-[15vh] md:h-[20vh]'>
                 <div>
                     <img src={lapler_logo} className='h-8'/>
                 </div>
-                <div className='flex items-center gap-12 text-gray-300 '>
+                <div className='hidden md:flex items-center gap-12 text-gray-300'>
                     {/* <p className='hover:text-white'>Docs</p> */}
                     <Link to='/login'>
                         <p className='hover:text-white'>
@@ -25,21 +25,21 @@ export default function VisitorHomepage() {
                     </Link>
                 </div>
             </div>
-            <div className='flex flex-col items-center justify-center bg-black w-full h-[65vh]'>
+            <div className='flex flex-col items-center justify-center bg-black w-full h-[55vh] md:h-[65vh]'>
                 
-                <h1 className='w-7/12 m-4 text-white text-center font-black text-3xl leading-tight tracking-tight'>
-                    The Issue Tracking Software You'll Love Using
+                <h1 className='md:8/12 lg:w-7/12 m-4 text-center font-black text-[2rem] lg:text-3xl md:text-3xl text-white leading-tight tracking-tight'>
+                    The Issue Tracking Software You'll Love <a className='hidden md:inline'>Using</a>
                 </h1>
-                <p className='w-1/2 text-center text-gray-400 text-m-lg leading-tight tracking-tight'>
+                <p className='md:w-7/12 md:text-center text-gray-400 leading-tight tracking-tight text-[1rem] md:text-m-lg px-10 text-justify'>
                     An open-source bug tracker for software teams that centralizes bug tracking, 
                     issue management, and feature requests. It includes customizable workflows, 
                     tagging, filtering, sorting, and integrates with version control systems.
                 </p>
 
-                <div className='flex gap-5 p-10'>
+                <div className='flex flex-col md:flex-row gap-5 p-10'>
                     <Link 
                         to='/register'
-                        className='flex items-center justify-center gap-2 text-black px-8 bg-white py-2.5 text-m-lg font-semibold rounded-md'
+                        className='flex md:flex-row items-center justify-center gap-2 text-black px-8 bg-white py-2.5 text-m-lg font-semibold rounded-md'
                     >
                         <img src={lightningIconBlack} className='h-6'/>
                         <p>Start Tracking</p>
@@ -54,10 +54,10 @@ export default function VisitorHomepage() {
                     </Link>
                 </div>
             </div>
-            <div className='flex justify-center pb-20 bg-gradient-to-b from-black to-blue-900'>
-                <img src={ui_display} className='rounded w-10/12 border border-blue-700' />
+            <div className='flex justify-center pb-12 md:pb-20 bg-gradient-to-b from-black to-blue-900'>
+                <img src={ui_display} className='rounded w-11/12 md:w-10/12 border border-blue-700' />
             </div>
-            <div className='flex items-center justify-between h-[20vh] bg-black px-[8vw]'>
+            <div className='flex items-center justify-between h-[10vh] md:h-[20vh] bg-black px-[8vw]'>
                 <div>
                     <img src={lapler_logo} className='h-8' />
                 </div>
