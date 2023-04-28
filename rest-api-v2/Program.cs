88c4e65d5616f3a200d9d4910e49f1f318f543d8
riz-j <adiputra979@gmail.com>
2023-04-29 00:50:41 +1000

    New database address

--- rest-api-v2/Program.cs
+++ rest-api-v2/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
-    option.UseNpgsql("Host=172.104.46.87:5432;Username=rizki;Password=arrahman;Database=lapler-api-v2");
+    option.UseNpgsql("Host=194.195.121.36:5432;Username=rizki;Password=arrahman;Database=lapler-api-v2");
 });
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
